f6a5d6aa4ddf91140e5ccbf9a1fd53a4f4d66e87
Bar Gabay <bar.gabay.home@gmail.com>
2022-11-02 13:06:05 +0200

    feat: more generics

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -17,12 +17,21 @@ const createPair = <firstElement, secondElement>(var1: firstElement, var2: secon
   return [var1, var2];
 }
 
-const pair = createPair(1, "2"); 
-console.log(`Pair: ${pair}`);
-console.log(`Type of pair: ${typeof(pair)}`);
+const pair1 = createPair(1, "2"); 
+console.log(`Pair: ${pair1}`);
+console.log(`Type of pair: ${typeof(pair1)}`);
 
 type Name<T> = { value: T };
 const name: Name<string> = { value: "Bar" };
 console.log(typeof(name), name.value);
+
+const createLoggedPair = <S extends string | number, T extends string | number>(v1: S, v2: T): [S, T] => {
+  console.log(`creating pair: v1='${v1}', v2='${v2}'`);
+  return [v1, v2];
+}
+
+const pair2 = createLoggedPair("Hi", "Amit");
+console.log(pair2, typeof(pair2));
+
 
 

e36a07039f2ded5976235006797364bcfac0f80a
Bar Gabay <bar.gabay.home@gmail.com>
2022-11-02 13:02:23 +0200

    feat: generics

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,10 +4,25 @@ export const validateObject = (input: object) => {
     const schema = Joi.object().keys({
       home: Joi.string().required(),
     });
-  return schema.validate(input).error;
+  return schema.validate(input).error ?
+   "Error: " + schema.validate(input).error?.details[0].message :
+    "valid";
 };
 
-console.log(validateObject({
-  home: "dfs",
-  hog: "afdsaf"
-}));
+// console.log(validateObject({
+//   dsa: "dfs",
+// }));
+
+const createPair = <firstElement, secondElement>(var1: firstElement, var2: secondElement): [firstElement, secondElement] => {
+  return [var1, var2];
+}
+
+const pair = createPair(1, "2"); 
+console.log(`Pair: ${pair}`);
+console.log(`Type of pair: ${typeof(pair)}`);
+
+type Name<T> = { value: T };
+const name: Name<string> = { value: "Bar" };
+console.log(typeof(name), name.value);
+
+
